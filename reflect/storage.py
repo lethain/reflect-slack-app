90755cde2f743a48591c920f827058099ca2db76
Will Larson <lethain@gmail.com>
2019-11-09 18:46:10 -0800

    Implement reflect

--- reflect/storage.py
+++ reflect/storage.py
@@ -1,8 +1,27 @@
-import random
+from google.cloud import firestore
+import datetime
+
+
+TASK_COLLECTION = 'tasks'
+
+db = firestore.Client()
+
+
+def tasks(team_id, user_id):
+    key = "%s:%s" % (team_id, user_id)
+    ref = db.collection('users').document(key).collection('tasks')
+    return ref
 
 
 def reflect(team_id, user_id, text):
-    print("Reflected(%s, %s): %s" % (team_id, user_id, text))
+    doc = {
+        'team': team_id,
+        'user': user_id,
+        'text': text,
+        'ts': datetime.datetime.now(),
+    }
+    col = tasks(team_id, user_id)
+    col.add(doc)
 
 
 def recall(team_id, user_id, text):
