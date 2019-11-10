4584819ae0fee8bd652cff3a4f20dd2ebf08b6e3
Will Larson <lethain@gmail.com>
2019-11-09 21:18:45 -0800

    Implement recall function to retrieve tasks in Firestore

--- reflect/storage.py
+++ reflect/storage.py
@@ -1,15 +1,13 @@
-from google.cloud import firestore
 import datetime
+from google.cloud import firestore
 
 
-TASK_COLLECTION = 'tasks'
-
-db = firestore.Client()
+DB = firestore.Client()
 
 
 def tasks(team_id, user_id):
     key = "%s:%s" % (team_id, user_id)
-    ref = db.collection('users').document(key).collection('tasks')
+    ref = DB.collection('users').document(key).collection('tasks')
     return ref
 
 
@@ -25,5 +23,6 @@ def reflect(team_id, user_id, text):
 
 
 def recall(team_id, user_id, text):
-    "Recall reflections for (`team_id`, `user_id`) filtered by parameters in `text`"
-    return ["I did something {}".format(x) for x in range(random.randint(3, 10))]
+    col = tasks(team_id, user_id)
+    for task in col.stream():
+        yield task.to_dict()['text']
